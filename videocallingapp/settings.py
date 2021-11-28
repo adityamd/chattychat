3ac152d153343ae5002319da8232e7a379ea3b6a
Aditya Tripathi <aditya.t2210@gmail.com>
2021-11-28 20:39:54 +0530

    Deployment code added

--- videocallingapp/settings.py
+++ videocallingapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-$j3(yn#d1*e9!)991qwdpv$^)y@hlh%=@s^dgjh9wy2bn0d&l1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chattychatpython.herokuapp.com','127.0.0.1']
 ASGI_APPLICATION = "myproject.asgi.application"
 
 
@@ -121,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
